ba624bfc787d3d6d30135bf3630259d71d022b26
Innovider <42913925+Innovider@users.noreply.github.com>
2022-10-22 15:39:32 +0600

    Addaptive cards

--- style.js
+++ style.js
@@ -1,3 +1,7 @@
 let hei = $('nav').height();
 
 $('main').css('margin',hei+10);
+
+let cardWidth = $('.card-item>div:nth-child(2)').width();
+
+$('.card-content-text > h2').css('font-size', cardWidth/100 * 5 + 'px');
